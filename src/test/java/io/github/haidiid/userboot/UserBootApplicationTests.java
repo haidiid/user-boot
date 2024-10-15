e9aa8fe1bb0c4594b0448223bb49f7a2a4ccc37e
Kasyfillah Haidi <haidi.id@hotmail.com>
2024-10-15 09:18:22 +0700

    VEHC-001: Initial commit

---
+++ src/test/java/io/github/haidiid/userboot/UserBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.haidiid.userboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
